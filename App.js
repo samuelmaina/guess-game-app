b50394e87d21fd14bc6a9be1710524542f19d694
samuelmayna <samuelmayna@gmail.com>
2022-02-03 10:56:17 +0300

    Removed the ScrollView and replaced with FlatList which are more effecient

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { render } from "react-dom";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -14,7 +23,10 @@ export default function App() {
 
   const addGoalHandler = () => {
     //use the arrow function to always ensure that  the current state is updated
-    setCourseGoals((currentState) => [...currentState, enteredGoal]);
+    setCourseGoals((currentState) => [
+      ...currentState,
+      { key: Math.random().toString() + enteredGoal, value: enteredGoal },
+    ]);
   };
   return (
     <View style={styles.screen}>
@@ -22,17 +34,35 @@ export default function App() {
         <TextInput
           placeholder="Course Goals"
           style={styles.input}
-          //used to handle each key stroke that the user typers.
+          //used to handle each key stroke that the user types.
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
         <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View>
+      {/* the view Component is not scrollable hence there is need to set
+      scrollable view */}
+      {/* scroll view is quite inefficient since it renders Components that are not
+      seen in the app. A better option is to use the flatlist */}
+      {/* <ScrollView>
         {courseGoals.map((goal) => {
-          return <Text>{goal}</Text>;
+          return (
+            <View style={styles.goalHolder}>
+              <Text key={goal + Math.floor(Math.random() * 100)}>{goal}</Text>
+            </View>
+          );
         })}
-      </View>
+      </ScrollView> */}
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => {
+          return (
+            <View style={styles.goalHolder}>
+              <Text>{itemData.item.value}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -51,5 +81,10 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
+  },
+  goalHolder: {
+    marginVertical: 10,
+    backgroundColor: "lightgrey",
+    height: 40,
   },
 });
